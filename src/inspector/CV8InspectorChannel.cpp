56a8251194fda54653e6e948437c23706ab9ac19
LeonMrBonnie <leonmrbonnie@hotmail.com>
2020-11-30 16:14:55 +0100

    Fix promise rejecting every time

--- src/inspector/CV8InspectorChannel.cpp
+++ src/inspector/CV8InspectorChannel.cpp
@@ -35,7 +35,7 @@ void CV8InspectorChannel::Send(int id, const v8_inspector::StringView& string)
         v8::Local<v8::Object> result = v8::JSON::Parse(context, message).ToLocalChecked().As<v8::Object>();
         v8::MaybeLocal<v8::Value> error = result->Get(context, v8::String::NewFromUtf8(isolate, "error").ToLocalChecked());
         v8::Local<v8::Value> errorObj;
-        if (error.ToLocal(&errorObj))
+        if (error.ToLocal(&errorObj) && errorObj.As<v8::Object>()->HasOwnProperty(context, v8::String::NewFromUtf8(isolate, "code").ToLocalChecked()).ToChecked())
             promise->Reject(context, errorObj);
         else
             promise->Resolve(context, result);
