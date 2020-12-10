6489bf5b80c3754842c0bc80ab69bc3f1aa1d6df
LeonMrBonnie <leonmrbonnie@hotmail.com>
2020-12-10 20:01:59 +0100

    Add check if protocol message is valid

--- src/inspector/CV8InspectorClient.cpp
+++ src/inspector/CV8InspectorClient.cpp
@@ -21,6 +21,15 @@ v8::Local<v8::Promise> CV8InspectorClient::SendInspectorMessage(v8::Isolate* iso
 {
     v8::HandleScope handle_scope(isolate);
 
+    std::unique_ptr<uint16_t[]> method_buffer(new uint16_t[method.GetSize()]);
+    v8::Local<v8::String> methodString = v8::String::NewFromUtf8(isolate, method.CStr()).ToLocalChecked();
+    methodString->Write(isolate, method_buffer, 0, method.GetSize());
+    v8_inspector::StringView method_view(method_buffer.get(), method.GetSize());
+    if (!v8_inspector::V8InspectorSession::canDispatchMethod(method_view)) {
+        V8Helpers::Throw(isolate, "Invalid protocol method passed");
+        return;
+    }
+
     v8::Local<v8::Context> ctx = isolate->GetCurrentContext();
     auto id = CV8InspectorClient::GetNextMessageId();
 
@@ -40,15 +49,15 @@ v8::Local<v8::Promise> CV8InspectorClient::SendInspectorMessage(v8::Isolate* iso
     sprintf_s(paramsChar, "{ \"id\": %d, \"method\": \"%s\", \"params\": %s }", id, method.CStr(), paramsString.c_str());
     v8::Local<v8::String> message = v8::String::NewFromUtf8(isolate, paramsChar).ToLocalChecked();
 
-    CV8ResourceImpl* resource = static_cast<CV8ResourceImpl*>(V8ResourceImpl::Get(ctx));
-    v8_inspector::V8InspectorSession* session = resource->GetInspector()->GetSession();
-
     std::unique_ptr<uint16_t[]> buffer(new uint16_t[message->Length()]);
     message->Write(isolate, buffer.get(), 0, message->Length());
 
     v8_inspector::StringView message_view(buffer.get(), message->Length());
     {
         v8::SealHandleScope seal_handle_scope(isolate);
+
+        CV8ResourceImpl* resource = static_cast<CV8ResourceImpl*>(V8ResourceImpl::Get(ctx));
+        v8_inspector::V8InspectorSession* session = resource->GetInspector()->GetSession();
         session->dispatchProtocolMessage(message_view);
     }
     return resolver->GetPromise();
