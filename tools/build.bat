400549b54e334532d071897e985c1f565a597696
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-04-01 20:48:23 +0200

    Add build script

---
+++ tools/build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+:: Build the project
+cmake . -BBUILD
+cmake --build BUILD --config Release
+
+:: Copy built binary to dist folder
+IF NOT EXIST dist (
+    mkdir dist
+)
+copy BUILD\Release\altv-client-js.dll dist
+copy BUILD\Release\altv-client-js.lib dist
+copy BUILD\Release\altv-client-js.pdb dist
+
+:: The extra.bat can be used to run an additional step after compilation,
+:: like copying the file to your client directory automatically
+IF EXIST extra.bat (
+    extra.bat
+)
